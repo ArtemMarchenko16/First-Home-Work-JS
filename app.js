75a38c991a47e14d503d4009b3d294bb536d8174
ArtemMarchenko16 <110462384+ArtemMarchenko16@users.noreply.github.com>
2023-11-21 11:50:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+let firstVariable = "hello";
+console.log(firstVariable);
+
+let secondVariable = "owu";
+console.log(secondVariable);
+
+let thirdVariable = "com";
+console.log(thirdVariable);
+
+let fourthVariable = "ua";
+console.log(fourthVariable);
+
+let fifthVariable = 1;
+console.log(fifthVariable);
+
+let sixthVariable = 10;
+console.log(sixthVariable);
+
+let seventhVariable = -999;
+console.log(seventhVariable);
+
+let eighthVariable = 123;
+console.log(eighthVariable);
+
+let ninthVariable = 3.14;
+console.log(ninthVariable);
+
+let tenthVariable = 2.7;
+console.log(tenthVariable);
+
+let eleventhVariable = 16;
+console.log(eleventhVariable);
+
+let twelfVariable = true;
+console.log(twelfVariable);
+
+let thirteenthVariable = false;
+console.log(thirteenthVariable);
+
+
+let firstName = "Marchenko";
+let middleName = "Artem";
+let lastName = "Dmytrovych";
+
+let unifier = firstName + " " + middleName + " " + lastName;
+console.log(unifier);
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+let name = prompt('Як тебе звати?');
+console.log(name);
+
+let secondName = prompt('Як тебе по-батькові?');
+console.log(secondName);
+
+let age = prompt('Скільки тобі років?');
+console.log(age);
